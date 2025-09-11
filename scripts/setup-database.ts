4fcb6757ecdbf1e682e3d32db1637783d17de774
Ennio Marke <mystical.ennio@gmail.com>
2025-09-11 20:24:29 +0200

    ts

--- scripts/setup-database.ts
+++ scripts/setup-database.ts
@@ -1,13 +1,9 @@
 import { neon } from '@neondatabase/serverless';
 
-const sql = neon(process.env.DATABASE_URL!);
-
 async function setupDatabase() {
     console.log('Setting up database tables...');
 
     try {
-        import { neon } from '@neondatabase/serverless';
-
         const sql = neon(process.env.DATABASE_URL!);
 
         async function createGalleryTable() {
